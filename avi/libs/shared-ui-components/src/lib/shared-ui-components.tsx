2e051adac1f13c8d2d6b86c6fd9f6f7ca9ef86f4
avimehenwal <avi.mehanwal@gmail.com>
2021-12-26 22:48:54 +0100

    build(nx): :heavy_plus_sign: new next library

---
+++ avi/libs/shared-ui-components/src/lib/shared-ui-components.tsx
@@ -0,0 +1,14 @@
+import './shared-ui-components.module.css';
+
+/* eslint-disable-next-line */
+export interface SharedUiComponentsProps {}
+
+export function SharedUiComponents(props: SharedUiComponentsProps) {
+  return (
+    <div>
+      <h1>Welcome to SharedUiComponents!</h1>
+    </div>
+  );
+}
+
+export default SharedUiComponents;
