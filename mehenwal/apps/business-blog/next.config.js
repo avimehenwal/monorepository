a31df04be9d81c57643886df1f0fb391d49d3044
avimehenwal <avi.mehanwal@gmail.com>
2021-12-31 00:06:20 +0100

    feat(next): :zap: add MDX support

--- mehenwal/apps/business-blog/next.config.js
+++ mehenwal/apps/business-blog/next.config.js
@@ -1,5 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -10,6 +17,8 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
 };
 
-module.exports = withNx(nextConfig);
+
+module.exports = withNx(withMDX(nextConfig));
---
+++ mehenwal/apps/business-blog/pages/test-mdx/index.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface TestMdxProps {}
+
+export function TestMdx(props: TestMdxProps) {
+  return (
+    <div>
+      <h1>Welcome to TestMdx!</h1>
+    </div>
+  );
+}
+
+export default TestMdx;
